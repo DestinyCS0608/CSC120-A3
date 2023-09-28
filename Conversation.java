b85e54a431cb317d2add620120a72998c9ef279b
DestinyCS0608 <dcecchisamuels@smith.edu>
2023-09-28 14:17:28 -0400

    Initial commit

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,82 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+/* Conversation chat box, allow input of number of rounds from user 
+and continues to chat with them */
+
+class Conversation {
   public static void main(String[] arguments) {
+    //Creating a transcript
+    ArrayList<String> transcipt = new ArrayList<String>(); // Create an ArrayList object for transcript
+
+    // Create a Scanner object
+    Scanner userInput = new Scanner(System.in);
+
+    //  It will first ask the user to choose a number of rounds of conversation. 
+    String rounds = "How many rounds?: ";
+    System.out.println(rounds);
+    int numRounds = userInput.nextInt();
+    userInput.nextLine(); 
+        
     // You will start the conversation here.
-  }
+    String greeting = "Hi there!  What's on your mind?";
+    System.out.println(greeting);
+    transcipt.add(greeting); //adds the greeting to the transcript
+
+    // loop for number of rounds
+
+    int i = 0; //iterator
+    String conversation = ""; //empty strig to hold the chat
+
+    for (int count = 0; count <= numRounds; count++){
+
+        String userResponse = userInput.nextLine();
+        transcipt.add(userResponse);
+
+        conversation = "";
+        String [] splitWords = userResponse.split(" ");
+        String [] userPronouns = {"I", "me", "you", "my", "your", "am", "are"};
+        String [] replacePronouns = {"you", "you", "me", "your", "my", "are", "am"};
+         //Array of canned responses
+        String[] canned = {"Uh-huh", "Interesting!", "Oh wow!", "Mmm-hm.", "Tell me more!"};
+        
+        Random random = new Random();
+        int randomIndex = random.nextInt(canned.length);
+        String randomResponse = canned[randomIndex];
+
+        for (int count2 = 0; count2 < splitWords.length; count2++){
+            String word = "";
+
+            for (int count3 = 0; count3 < userPronouns.length; count3 ++){
+                if (splitWords[count2].equalsIgnoreCase(userPronouns[count3])){
+                    i++;
+                    word = replacePronouns[count3];
+                }
+            }
+            if (word.equals("")){
+                conversation += splitWords + " ";
+            } else {
+                conversation += word + " ";
+            }
+
+        }
+        if (i > 0) {
+            System.out.println(conversation);
+            transcipt.add(conversation); 
+        }
+        if (i <= 0){
+            System.out.println(randomResponse);
+            transcipt.add(randomResponse);
+    }
+        }
+        userInput.close();
+
+    System.out.println("Bye Bye!");
+    System.out.print(transcipt);
+    }
 }
+
+//* Sources: .https://www.w3schools.com/java/java_arraylist.asp *//
+// https://www.baeldung.com/string/split#:~:text=split(),-Last%20updated%3A%20August&text=The%20method%20split()%20splits,which%20contains%20the%20split%20Strings.
+// https://www.w3schools.com/java/ref_string_equalsignorecase.asp#:~:text=The%20equalsIgnoreCase()%20method%20compares,strings%20lexicographically%2C%20ignoring%20case%20differences.
--- reflection.md
+++ reflection.md
@@ -5,3 +5,6 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+I consoled Katie Vogt and Ruth Kendall. It was nice to talk to them about the assignment and just talk about any confusion we had. I didn't attend any of the office hours. I stuggle with stacked tasks and executive fucntion so getting up and asking for help would probably be the last things I do. 
+Java has grown on me for sure. I thought it was impossible to learn but now I am able to read some minor code without having to go to geeks for geeks. I am still struggling with my code to work. I am able to run the code and replace the pronouns but any words after that the console outputs stuff like "Ljava.lang.String;@17c68925" I tried my best. But I think I have to step away for now.
